83edc5f2dd2ef027a9f1080dcbdb28c6c6b8d281
Narayan5876 <naran5876@gmail.com>
2020-01-18 16:51:15 +0545

    hotel finder and room reservation backend

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+require('./database/mongoose')
+const app = express()
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}))
+const userrouter = require('./router/user')
+const cors = require('cors')
+app.use(cors())
+const productrouter = require('./router/product')
+
+
+
+
+
+app.use(express.json())
+app.use(userrouter)
+
+
+//app.use(taskrouter)
+
+app.listen(3000);
+//this is our port 
